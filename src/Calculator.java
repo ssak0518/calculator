45bcd0cf9c462ada0e37699bd6bc3752c295328f
simyj <simyj@172.30.1.2>
2021-07-06 15:23:50 +0900

    사칙연산 기호에 따른 기능 추가

--- src/Calculator.java
+++ src/Calculator.java
@@ -7,13 +7,25 @@ public class Calculator {
 		int first = scanner.nextInt();
 		System.out.println(first);
 		
+		System.out.println("사칙연산 기호 : ");
+		String symbol = scanner.next();
+		System.out.println(symbol);
+		
 		System.out.println("두번째 입력 값: ");
 		int second = scanner.nextInt();
 		System.out.println(second);
 		
-		System.out.println("덧셈 :" + (first + second));
-		System.out.println("뺄셈 :" + (first - second));
-		System.out.println("곱셈 :" + (first * second));
-		System.out.println("나눗셈 :" + (first / second));
+		if (symbol.equals("+")) {
+			System.out.println("덧셈 :" + (first + second));
+		} else if (symbol.equals("-")) {
+			System.out.println("뺄셈 :" + (first - second));
+		} else if (symbol.equals("*")) {
+			System.out.println("곱셈 :" + (first * second));
+		} else if (symbol.equals("/")) {
+			System.out.println("나눗셈 :" + (first / second));
+		} else {
+			System.out.println("사칙연산 기호가 아닙니다.");
+		}
+				
 	}
 }
